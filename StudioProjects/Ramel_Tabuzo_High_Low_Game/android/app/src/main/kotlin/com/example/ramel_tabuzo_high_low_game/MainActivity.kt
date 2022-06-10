be02bedcd8c1f98a1e8a4fb1024a635757064d6c
Ramel <ramel.tabuzo@bisu.edu.ph>
2022-06-10 15:21:48 +0800

    first commit

--- StudioProjects/Ramel_Tabuzo_High_Low_Game/android/app/src/main/kotlin/com/example/ramel_tabuzo_high_low_game/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ramel_tabuzo_high_low_game
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- StudioProjects/Ramel_Tabuzo_High_Low_Game/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:ramel_tabuzo_high_low_game/desktopmobile.dart';
-
-void main() => runApp( MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: chooseMobileDesktop(),
-));
